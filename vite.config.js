876d3cfb5ea82f9c451bdb8f60a7a9126ec53be9
Aashh karwade <ashwinkarwade54321@gmail.com>
2024-09-26 21:04:11 +0530

    error fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bade:"/ng-training-To-Do-List/"
+  bade:"/ng-training-To-Do-List"
 })
